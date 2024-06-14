41ed10fdc3caeabc3aa07d0746d6edb3e8fe515e
Beza Tameru <bbt0002@auburn.edu>
2024-06-13 21:03:03 -0500

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Bezawit Tameru on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
